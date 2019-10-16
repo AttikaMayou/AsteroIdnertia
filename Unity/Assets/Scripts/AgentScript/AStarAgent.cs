b1165650ca6bb164c5f58f57e1dd97dd3237b2e6
Attika <attika.mayou@gmail.com>
2019-10-16 23:59:58 +0200

    WIP AStar

--- Unity/Assets/Scripts/AgentScript/AStarAgent.cs
+++ Unity/Assets/Scripts/AgentScript/AStarAgent.cs
@@ -86,9 +86,7 @@ public class AStarAgent : IAgent
         public float CalculateFrames()
         {
             float distProjectilePlayer = math.distance(playerPos, projectilePos);
-            float frames = gs.currentGameStep + distProjectilePlayer/gameParameters.ProjectileSpeed;
-
-            return frames;
+            return gs.currentGameStep + distProjectilePlayer/gameParameters.ProjectileSpeed;
         }
     }
 }
