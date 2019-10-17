032221e924cadd8e2d312a02f84bf88f6cc4bf36
ArthurJacquin <artuj38@free.fr>
2019-10-18 00:38:29 +0200

    WIP MLAgent

--- Unity/Assets/Scripts/AgentScript/MLAgent.cs
+++ Unity/Assets/Scripts/AgentScript/MLAgent.cs
@@ -3,6 +3,7 @@ using Unity.Jobs;
 using Random = Unity.Mathematics.Random;
 using Rules = GameStateRules;
 using UnityEngine;
+using Unity.Mathematics;
 
 public class MLAgent : IAgent
 {
@@ -13,7 +14,17 @@ public class MLAgent : IAgent
             availableActions = availableActions,
             gs = gs,
             rdmAgent = new RandomAgent { rdm = new Random((uint)Time.frameCount + (uint)playerId) },
+            intermediateSteps = new NativeList<float2>(10, Allocator.TempJob),
+            playerId = playerId,
+            gameParameters = GameParameters.Instance.Parameters,
+            summedScores = new NativeArray<long>(availableActions.Length, Allocator.TempJob)
         };
+
+        var handleLong = longJob.Schedule(availableActions.Length, 1);
+        handleLong.Complete();
+
+        NativeList<float2> stepsToDo = new NativeList<float2>(longJob.intermediateSteps.Length, Allocator.Temp);
+        stepsToDo.AddRange(longJob.intermediateSteps);
 
         ActionsTypes choosenAction = availableActions[0];
         return choosenAction;
@@ -30,15 +41,72 @@ public class MLAgent : IAgent
 
         public RandomAgent rdmAgent;
 
-        //[WriteOnly]
-        //Type de retour = les étapes intermédiaires == quelle structure ??
-        //En faire une structure justement ?
+        [WriteOnly]
+        public NativeList<float2> intermediateSteps;
+
+        public NativeArray<long> summedScores;
 
         public int playerId;
 
         public void Execute(int index)
         {
-            throw new System.NotImplementedException();
+            var epochs = 100;
+            var agent = rdmAgent;
+
+            var gsCopy = Rules.Clone(ref gs);
+
+            for (var n = 0; n < epochs; n++)
+            {
+                Rules.CopyTo(ref gs, ref gsCopy);
+                Rules.Step(ref gameParameters, ref gsCopy, availableActions[index], 0);
+
+                var currentDepth = 0;
+                var maxIteration = 1000;
+
+                while (currentDepth < maxIteration || !gsCopy.players[1].isGameOver)
+                {
+                    Rules.Step(ref gameParameters, ref gsCopy,
+                    agent.Act(ref gsCopy, availableActions, 0),
+                    agent.Act(ref gsCopy, availableActions, 1));
+                    currentDepth++;
+                }
+
+                summedScores[index] += gsCopy.players[playerId].score;
+                gsCopy.projectiles.Dispose();
+                gsCopy.asteroids.Dispose();
+            }
+
+            int bestActionIndex = -1;
+            var bestScore = long.MinValue;
+
+            for (var i = 0; i < summedScores.Length; i++)
+            {
+                if (bestScore > summedScores[i])
+                {
+                    continue;
+                }
+                //  Debug.Log(availableActions[i] + " : " + job.summedScores[i]);
+                bestScore = summedScores[i];
+                bestActionIndex = i;
+            }
+            // Check if all value are same
+            var value = summedScores[0];
+            bool same = true;
+            for (int i = 1; i < summedScores.Length; i++)
+            {
+                same = summedScores[i] == value ? true : false;
+                if (!same)
+                {
+                    break;
+                }
+            }
+
+            ActionsTypes chosenAction;
+            if (!same)
+                chosenAction = availableActions[bestActionIndex];
+            else
+                chosenAction = rdmAgent.Act(ref gs, availableActions);
+            summedScores.Dispose();
         }
     }
 }
